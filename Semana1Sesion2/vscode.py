9847e0247706f94c24b797e056073bf2a6699600
Omar2389 <julio.milla89@gmail.com>
2020-06-07 12:00:21 -0500

    Agregando texto

---
+++ Semana1Sesion2/vscode.py
@@ -0,0 +1,17 @@
+## Desarrollo BACKEND2020
+
+Esta es mi carpeta de trabajo del curso de **Programación BACKEND** .
+
+
+## Contenido
+**Semana1 Sesión1**
+ - Nada
+
+**Semana1 Sesión2**
+ - Configuración de **VSCode**
+
+**Semana1 Sesión3**
+ - Git
+
+**Semana1 Hackaton**
+ - Configurar GitHub y GitLab
